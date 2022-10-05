3bc4edc49aa8ce6da0fe336e3ef7a478ae041e81
Thibault H <Aelto@users.noreply.github.com>
2022-10-05 17:58:43 +0200

    `actix` feature fixes & missing `pub` on important types (#2)
    - fixed compilation errors coming from the actix feature's code
    - made some missing types public, such as the RpcResult type and the error types

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod errors;
 mod message;
 mod response;
-mod rpc;
 mod surreal_client;
 
 pub use message::SurrealMessage;
 pub use response::SurrealResponse;
 pub use surreal_client::SurrealClient;
+pub mod errors;
+pub mod rpc;
